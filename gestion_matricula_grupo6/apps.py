5b38416961243fbd31b022ea8ee714336671f8c8
Roberto Eustaquio Jiménez <reusjimenez2002@gmail.com>
2024-07-21 21:32:16 -0500

    Inicializar el proyecto Django matricula_colegio_grupo6

---
+++ gestion_matricula_grupo6/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionMatriculaGrupo6Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestion_matricula_grupo6'
