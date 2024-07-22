efc5a87450f326022ac3ed81bd24db6404e72658
Roberto Eustaquio Jiménez <reusjimenez2002@gmail.com>
2024-07-22 03:08:29 -0500

    Cuarto  avance

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,9 @@ class AlumnoForm(forms.ModelForm):
     class Meta:
         model = Alumno
         fields = ['nombre', 'apellido', 'fecha_nacimiento', 'documento_identidad', 'es_nuevo']
+        widgets = {
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 class DocumentacionAdicionalForm(forms.ModelForm):
     class Meta:
